f81705847686d00c9215ab29952a3c787000f181
gavinmoore36 <gavizoid16@gmail.com>
2019-03-25 16:00:38 -0700

    Done

--- count_elements.rb
+++ count_elements.rb
@@ -1,4 +1,13 @@
 def count_elements(array)
   # code goes here
+  hash = new_hash(array)
+  array.each {|x| hash[x] += 1}
+  hash
 end
- 
+
+
+ def new_hash(array)
+  hash = Hash.new
+  array.each {|x| hash[x] = 0}
+  hash
+end
